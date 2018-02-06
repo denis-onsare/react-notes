a3126b03d9467f2a23e721457e3d485107eb14c0
denis-onsare <donnsare@gmail.com>
2018-02-06 23:04:56 +0300

    Add note component

---
+++ src/Note.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Note extends React.Component {
+	render () {
+		return (
+			<div>
+				hello from note component
+			</div>
+		);
+	}
+}
+
+export default Note;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './App.css';
+
+import Note from './Note';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Note />
+	, document.getElementById('root'));
+registerServiceWorker();
