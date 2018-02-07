eddc7852aead283de0dd565bd85c847ced338941
denis-onsare <donnsare@gmail.com>
2018-02-07 23:28:26 +0300

    Note component

--- src/Note.js
+++ src/Note.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class Note extends React.Component {
 	render () {
 		return (
-			<div>
-				hello from note component
+			<div className="note">
+				<h1>Hello world</h1>
+				<span className="glyphicon glyphicon-trash"></span>
+				<span className="glyphicon glyphicon-trash"></span>
 			</div>
 		);
 	}
